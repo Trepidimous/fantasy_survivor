8cb91553d0b69f7e02858a39bfa593d938bba73a
Neo <davehahnert@gmail.com>
2026-01-11 19:40:24 -0500

    [refactoring] organizing functino order. (Klean Kode Step down rule).

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -28,113 +28,6 @@ fn app() -> Html
 	print_html(&user_state, &message, &users, get_users.clone(), create_user.clone(), update_user.clone(), delete_user.clone(), edit_user.clone())
 }
 
-fn print_html(user_state: &UseStateHandle<(String, String, String, Option<i32>)>,
-	message: &UseStateHandle<String>,
-	users: &UseStateHandle<Vec<User>>,
-	get_users: Callback<()>,
-	create_user: yew::Callback<yew::MouseEvent>,
-	update_user: Callback<MouseEvent>,
-	delete_user: Callback<i32>,
-	edit_user: Callback<i32>) -> Html
-{
-	html!
-	{
-		<body class="bg-[#121212]  min-h-screen">
-			<div class="container mx-auto p-4">
-				<h1 class="text-4xl font-bold text-[#FF8C00] mb-4">{ "Game Master Portal" }</h1>
-					<div class="mb-4">
-						<input placeholder="Name"
-							value={user_state.0.clone()}
-							oninput={Callback::from(
-							{
-								let user_state = user_state.clone();
-								move |e: InputEvent|
-								{
-									let input = e.target_dyn_into::<web_sys::HtmlInputElement>().unwrap();
-									user_state.set((input.value(), user_state.1.clone(), user_state.2.clone(), user_state.3));
-								}
-							})}
-							class="border rounded px-4 py-2 mr-2"/>
-						<input placeholder="Email"
-							value={user_state.1.clone()}
-							oninput={Callback::from(
-							{
-								let user_state = user_state.clone();
-								move |e: InputEvent|
-								{
-									let input = e.target_dyn_into::<web_sys::HtmlInputElement>().unwrap();
-									user_state.set((user_state.0.clone(), input.value(), user_state.2.clone(), user_state.3));
-								}
-							})}
-							class="border rounded px-4 py-2 mr-2"/>
-
-						<button
-							onclick=
-							{
-								if user_state.3.is_some()
-								{
-									update_user.clone()
-								}
-								else
-								{
-									create_user.clone()
-								}
-							}
-							class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-							{ 
-								if user_state.3.is_some()
-								{
-									"Update User"
-								}
-								else
-								{ 
-									"Create User"
-								}
-							}
-							
-						</button>
-							if !message.is_empty()
-							{
-								<p class="text-green-500 mt-2">{ &**message }</p>
-							}
-					</div>
-
-					<button
-						onclick={get_users.reform(|_| ())}
-						class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded mb-4">
-						{ "Fetch User List" }
-					</button>
-
-					<h2 class="text-2xl font-bold text-[#FF8C00] mb-2">{ "User List" }</h2>
-
-					<ul class="list-disc pl-5">
-					{
-						for (*users).iter().map(|user|
-						{
-							let user_id = user.id;
-							html!
-							{
-								<li class="mb-2">
-								<span class="font-semibold text-[#4a90e2]">{ format!("ID: {}, Name: {}, Email: {} AccountType: {} ", user.id, user.name, user.email, user.account_type) }</span>
-								<button
-									onclick={edit_user.clone().reform(move |_| user_id)}
-									class="ml-4 bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-1 px-2 rounded">
-									{ "Edit" }
-								</button>
-								<button
-									onclick={delete_user.clone().reform(move |_| user_id)}
-									class="ml-4 bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded">
-									{ "Delete" }
-								</button>
-							</li>
-						}
-					})}
-				</ul>
-			</div>
-		</body>
-	}
-}
-
 fn get_users(users: &UseStateHandle<Vec<User>>,
 	message: &UseStateHandle<String>) -> Callback<()>
 {
@@ -295,6 +188,113 @@ fn edit_user(user_state : &UseStateHandle<(String, String, String, Option<i32>)>
 			}
 		})
 	};
+}
+
+fn print_html(user_state: &UseStateHandle<(String, String, String, Option<i32>)>,
+	message: &UseStateHandle<String>,
+	users: &UseStateHandle<Vec<User>>,
+	get_users: Callback<()>,
+	create_user: yew::Callback<yew::MouseEvent>,
+	update_user: Callback<MouseEvent>,
+	delete_user: Callback<i32>,
+	edit_user: Callback<i32>) -> Html
+{
+	html!
+	{
+		<body class="bg-[#121212]  min-h-screen">
+			<div class="container mx-auto p-4">
+				<h1 class="text-4xl font-bold text-[#FF8C00] mb-4">{ "Game Master Portal" }</h1>
+					<div class="mb-4">
+						<input placeholder="Name"
+							value={user_state.0.clone()}
+							oninput={Callback::from(
+							{
+								let user_state = user_state.clone();
+								move |e: InputEvent|
+								{
+									let input = e.target_dyn_into::<web_sys::HtmlInputElement>().unwrap();
+									user_state.set((input.value(), user_state.1.clone(), user_state.2.clone(), user_state.3));
+								}
+							})}
+							class="border rounded px-4 py-2 mr-2"/>
+						<input placeholder="Email"
+							value={user_state.1.clone()}
+							oninput={Callback::from(
+							{
+								let user_state = user_state.clone();
+								move |e: InputEvent|
+								{
+									let input = e.target_dyn_into::<web_sys::HtmlInputElement>().unwrap();
+									user_state.set((user_state.0.clone(), input.value(), user_state.2.clone(), user_state.3));
+								}
+							})}
+							class="border rounded px-4 py-2 mr-2"/>
+
+						<button
+							onclick=
+							{
+								if user_state.3.is_some()
+								{
+									update_user.clone()
+								}
+								else
+								{
+									create_user.clone()
+								}
+							}
+							class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+							{ 
+								if user_state.3.is_some()
+								{
+									"Update User"
+								}
+								else
+								{ 
+									"Create User"
+								}
+							}
+							
+						</button>
+							if !message.is_empty()
+							{
+								<p class="text-green-500 mt-2">{ &**message }</p>
+							}
+					</div>
+
+					<button
+						onclick={get_users.reform(|_| ())}
+						class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded mb-4">
+						{ "Fetch User List" }
+					</button>
+
+					<h2 class="text-2xl font-bold text-[#FF8C00] mb-2">{ "User List" }</h2>
+
+					<ul class="list-disc pl-5">
+					{
+						for (*users).iter().map(|user|
+						{
+							let user_id = user.id;
+							html!
+							{
+								<li class="mb-2">
+								<span class="font-semibold text-[#4a90e2]">{ format!("ID: {}, Name: {}, Email: {} AccountType: {} ", user.id, user.name, user.email, user.account_type) }</span>
+								<button
+									onclick={edit_user.clone().reform(move |_| user_id)}
+									class="ml-4 bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-1 px-2 rounded">
+									{ "Edit" }
+								</button>
+								<button
+									onclick={delete_user.clone().reform(move |_| user_id)}
+									class="ml-4 bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded">
+									{ "Delete" }
+								</button>
+							</li>
+						}
+					})}
+				</ul>
+			</div>
+		</body>
+	}
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
